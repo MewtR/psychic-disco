c2db5bae7319e7c8ac0fb63ac379e8013ceee907
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2021-02-16 14:24:56 -0500

    Basic create window example. Doesn't seem to work that well due to the tiling of bspwm.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,35 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    xcb_connection_t *c;
+    xcb_screen_t *screen;
+    xcb_window_t win;
+
+    /* open the connection to the X server */
+    c = xcb_connect(NULL, NULL);
+
+    /* Get the first screen */
+    screen = xcb_setup_roots_iterator(xcb_get_setup(c)).data;
+
+    /* Ask for our window's Id*/
+    win = xcb_generate_id(c);
+    /* Create the window */
+    xcb_create_window(c,
+                    XCB_COPY_FROM_PARENT,
+                    win,
+                    screen->root,
+                    0,0,
+                    150,150,
+                    10,
+                    XCB_WINDOW_CLASS_INPUT_OUTPUT,
+                    screen->root_visual,
+                    0, NULL);
+    /* Map the window on the screen (this is what makes it show up)*/
+    xcb_map_window(c,win);
+    /* Make sure commands are sent before we pause, so window is shown */
+    xcb_flush(c);
+
+    pause(); // hold client until Ctrl-C
+
     return 0;
 }
