1a3d4251250367bd2a9072277b6c156b3ee97cce
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2021-03-06 10:09:51 -0500

    Grabbing and obscured window doesn't even require redirecting the window and creating a pixmap. Having picom running is sufficient it seems.

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
     const xcb_setup_t* setup;
 
     // 1) Time it to see how long it takes
-    // 2) Can it take scrot of ff on a different workspace
+    // 2) Can it take scrot of ff on a different workspace. It can't. Need to find a way to do it
     // Initialize ImageMagick lib
     Magick::InitializeMagick(*argv);
     // open the connection to the X server 
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 
     xcb_window_t firefox = get_child(c, screen->root, XCB_ATOM_WM_CLASS, "Navigator");
     // request window redirect (draw offscreen)
-    xcb_composite_redirect_window(c, firefox, XCB_COMPOSITE_REDIRECT_AUTOMATIC);
+    //xcb_composite_redirect_window(c, firefox, XCB_COMPOSITE_REDIRECT_AUTOMATIC);
     std::cout << "Firefox window id is: "<< firefox << std::endl;
     std::shared_ptr<xcb_get_geometry_reply_t> geometry = get_geometry(c,firefox);
     std::cout << "Window's x: " << geometry->x << std::endl;
@@ -33,37 +33,22 @@ int main(int argc, char **argv)
     std::cout << "Version is " << version->major_version << "." << version->minor_version << std::endl;
 
     // create a pixmap
+    /*
     xcb_pixmap_t ff_pixmap = xcb_generate_id(c);
     xcb_composite_name_window_pixmap(c, firefox, ff_pixmap);
+    */
 
-    std::shared_ptr<xcb_get_image_reply_t> image = get_image(c, ff_pixmap, geometry);
+    std::shared_ptr<xcb_get_image_reply_t> image = get_image(c, firefox, geometry);
     // image length
     int len = xcb_get_image_data_length(image.get());
     // Image data. Not something that can be freed, lives on the stack apparently
     std::shared_ptr<u_int8_t> data (xcb_get_image_data(image.get()), [](uint8_t*){}); // empty deleter
-    // Image end
-    xcb_generic_iterator_t end = xcb_get_image_data_end(image.get());
-    //std::cout << "Printing out my pixels yall" << std::endl;
     
-    /*
-    uint8_t* it = data.get();
-    while(it != end.data)
-    {
-        std::cout << static_cast<int>(*it) << " ";
-        it++;
-    }
-    std::cout << std::endl;
-    
-    std::cout << "Window's width: " << geometry->width << std::endl;
-    std::cout << "Window's height: " << geometry->height << std::endl;
-    std::cout << "Length of image " << len << std::endl;
-    */
-
     Magick::Image final_image;
     final_image.read(geometry->width, geometry->height, "BGRA", Magick::CharPixel, data.get()); // Can't use 'it' here because I incremented it to print
     final_image.write("firefox.png");
 
-    xcb_free_pixmap(c, ff_pixmap);
+    //xcb_free_pixmap(c, ff_pixmap);
 
     xcb_disconnect(c);
 
